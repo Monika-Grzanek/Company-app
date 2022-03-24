5fcf16304254e1d60f4093107ad12bbd13947e28
Monika Grzanek <monski@o2.pl>
2022-03-24 18:45:20 +0100

    Add tests to department and employee

---
+++ models/test/employee.test.js
@@ -0,0 +1,37 @@
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+const Employee = require('./../employee.model.js');
+
+describe('Employee', () => {
+    after(() => {
+        mongoose.models = {};
+    });
+
+    it('should throw an error if no "firstName",  "lastName", "department" args', () => {
+        const emp = new Employee({});
+        emp.validate(err => {
+            expect(err.errors.firstName).to.exist;
+            expect(err.errors.lastName).to.exist;
+            expect(err.errors.department).to.exist;
+        })
+    });
+
+    it('should throw an error if "firstName", "lastName", "deparment" is not a string', () => {
+        const cases = [[], {}];
+        for(let item of cases) {
+            const emp = new Employee({ item });
+            emp.validate(err => {
+                expect(err.errors.firstName).to.exist;
+                expect(err.errors.lastName).to.exist;
+                expect(err.errors.department).to.exist;
+            })
+        }
+    });
+
+    it('should not throw an error if "name" is properly', () => { 
+        const emp = new Employee({ firstName: "John", lastName: "Doe", department: "IT" });
+        emp.validate(err => {
+            expect(err).to.not.exist;
+        })
+    })
+})
