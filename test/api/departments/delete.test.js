3004c6b541e9f34cb2b4b165c9e76a576c3a2892
Monika Grzanek <monski@o2.pl>
2022-03-25 23:08:31 +0100

    Add endpoint tests

---
+++ test/api/departments/delete.test.js
@@ -0,0 +1,28 @@
+const server = require('./../../../server');
+const Department = require('./../../../models/department.model');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('DELETE/api/departments', () => {
+    before(async () => {
+        const testDepOne = new Department({ _id: '5d9f1140f10a81216cfd4408', name: 'Department #1' });
+        await testDepOne.save();
+    });
+
+    it('/ should removed documents and return success', async () => {
+        const res = await request(server).delete('/api/departments/5d9f1140f10a81216cfd4408');
+        const removedDep = await Department.findOne({ name: 'Department #1'});
+        expect(res.status).to.be.equal(200);
+        expect(removedDep).to.be.null;
+    })
+
+    after(async () => {
+        await Department.deleteMany();
+    });
+
+})
